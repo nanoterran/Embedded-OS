c1e890d65608da91721f3dc040863a82088a3209
Javier <nanoterran@yahoo.com>
2019-10-26 13:29:25 -0400

    Adding main logic

--- Morse/main.c
+++ Morse/main.c
@@ -3,6 +3,16 @@
 
 int main(int argc, char *argv[])
 {
+	u_int8_t MaxNumberOfArguments = 3;
+
+	if(argc < MaxNumberOfArguments)
+	{
+		fprintf(stderr, "[-] ERROR: Insufficient number of arguments.");
+		fprintf(stderr, "[!] Usage: %s -w <Word>\n", argv[0]);
+		return 1;
+	}
+	char *raw_word = argv[2];
+	process_word(raw_word);
 
 	return 0;
 }
