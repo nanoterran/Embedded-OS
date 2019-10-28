5d7f5eef732bbd1c005e6ef9e32a8b01775285fe
Javier Vega <nanoterran@yahoo.com>
2019-10-28 19:22:26 -0400

    Utilities working

--- Morse/Utils.c
+++ Morse/Utils.c
@@ -1,23 +1,77 @@
 #include "Utils.h"
 
-void process_word(char *word)
+void process_word(char *word, char *architecture)
 {
-	int i;
-	for(i = 0; i < strlen(word); i++)
+	if(!strcmp(architecture, "x86_64\n"))
 	{
-		int ascii = (int)word[i];
-		char *mcode_letter = (char *)mcodestring(ascii);
-		process_mcode_letter(mcode_letter);
-		usleep(100000);
+		x86_64_process_mcode(word);
+	}
+	else if(!strcmp(architecture, "armv7l\n"))
+	{
+		arm_process_mcode(word);
 	}
 }
 
-void process_mcode_letter(char *mcode_letter)
+void arm_process_mcode(char *word)
 {
-	for(int i = 0; i < strlen(mcode_letter); i++)
+	const char *LEDBrightness = "/sys/class/leds/beaglebone:green:usr3/brightness";
+	FILE *file_descriptor = fopen(LEDBrightness, "r+");
+
+	for(int i = 0; i < strlen(word); i++)
 	{
-		printf("%c ", mcode_letter[i]);
-		usleep(50000);
+		int ascii_code = (int)word[i];
+		char *mcode_letter = (char *)mcodestring(ascii_code);
+		
+		for(int i = 0; i < strlen(mcode_letter); i++)
+		{
+			if(mcode_letter[i] == '.')
+			{
+				fprintf(file_descriptor, "1");
+				fflush(file_descriptor);
+				usleep(DotTimeInMicroSec);
+			}
+			else if(mcode_letter[i] == '-')
+			{
+				fprintf(file_descriptor, "1");
+				fflush(file_descriptor);
+				usleep(DashTimeInMicroSec);
+			}
+			fprintf(file_descriptor, "0");
+			fflush(file_descriptor);
+			usleep(BetweenCharacterTimeInMicroSec);
+		}
+		usleep(BetweenLetterTimeInMicroSec);
 	}
-	printf("\t");
+
+	fclose(file_descriptor);
+}
+
+void x86_64_process_mcode(char *word)
+{
+	for(int i = 0; i < strlen(word); i++)
+	{
+		int ascii_code = (int)word[i];
+		char *mcode_letter = (char *)mcodestring(ascii_code);
+		
+		for(int i = 0; i < strlen(mcode_letter); i++)
+		{
+			if(mcode_letter[i] == '.')
+			{
+				fprintf(stdout, "Dot ");
+				fflush(stdout);
+				usleep(DotTimeInMicroSec);
+			}
+			else if(mcode_letter[i] == '-')
+			{
+				fprintf(stdout, "Dash ");
+				fflush(stdout);
+				usleep(DashTimeInMicroSec);
+			}
+
+			usleep(BetweenCharacterTimeInMicroSec);
+		}
+
+		fprintf(stdout, "\t");
+		usleep(BetweenLetterTimeInMicroSec);
+	}
 }
--- Morse/Utils.h
+++ Morse/Utils.h
@@ -7,7 +7,16 @@
 #include <unistd.h>
 #include "McodeMod.h"
 
-void process_mcode_letter(char *mcode_letter);
-void process_word(char *word);
+enum
+{
+  DotTimeInMicroSec =              500000,
+  DashTimeInMicroSec =             1500000,
+  BetweenCharacterTimeInMicroSec = 250000,
+  BetweenLetterTimeInMicroSec =    2000000
+};
+
+void x86_64_process_mcode(char *word);
+void arm_process_mcode(char *word);
+void process_word(char *word, char *architecture);
 
 #endif
