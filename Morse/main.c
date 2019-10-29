6037a0922583602bfce30915a7effc06c1cae74e
Javier Vega <nanoterran@yahoo.com>
2019-10-28 23:02:20 -0400

    Working

--- Morse/main.c
+++ Morse/main.c
@@ -10,20 +10,44 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "[!] Usage: %s -w <Word>\n", argv[0]);
 		return 1;
 	}
-	char *raw_word = argv[2];
 
+	char architecture[10];
+	mcode_configuration_t *configuration = malloc(sizeof(configuration));
+
+	// Find the word on the argument options
+	for(int i = 0; i < argc; i++)
+	{
+		if(!strcmp(argv[i], "-w"))
+		{
+			configuration->raw_word = argv[i + 1];
+		}
+	}
+
+	// Gets the architecture of the system
 	FILE *command_fd = popen("uname -m", "r");
 	if (command_fd == NULL) {
     printf("Failed to run command\n" );
     exit(1);
   }
-
-	char architecture[10];
-
-	/* Read the output a line at a time - output it. */
   fgets(architecture, sizeof(architecture), command_fd);
 	fclose(command_fd);
-	process_word(raw_word, architecture);
+
+	configuration->architecture = architecture;
+
+	if(!strcmp(configuration->architecture, "x86_64\n"))
+	{
+		configuration->file_descriptor = stdout;
+	}
+	else if(!strcmp(configuration->architecture, "armv7l\n"))
+	{
+		const char *LEDBrightness = "/sys/class/leds/beaglebone:green:usr3/brightness";
+		configuration->file_descriptor = fopen(LEDBrightness, "r+");;
+	}
+
+	process_word(configuration);
+
+	fclose(configuration->file_descriptor);
+	free(configuration);
 
 	return 0;
 }
