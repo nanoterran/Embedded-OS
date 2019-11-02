e8c4ce666afa376f6296a6b01bc115437f27c6e4
Javier Vega <nanoterran@yahoo.com>
2019-11-02 14:43:30 -0400

    Finding task pid by name

--- FindTask/FindTask.c
+++ FindTask/FindTask.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/string.h>
 #include <linux/sched/signal.h>   //Includes for_each_process()
 
 MODULE_LICENSE("GPL");
@@ -9,13 +10,23 @@ MODULE_AUTHOR("Javier Vega");
 MODULE_DESCRIPTION("Finds a task.");
 MODULE_VERSION("1.00");
 
+static char *name = "none";
+module_param(name, charp, S_IRUGO);
+MODULE_PARM_DESC(name, "The name of the process to find");  
+
 static int __init find_task_init(void) {
   struct task_struct *task_list;
 
+  // Traverses the list of tasks until we find the process we are looking for.
   for_each_process(task_list) {
-    printk(KERN_INFO "%s: %d\n", task_list->comm, task_list->pid);
+    if(!strcmp(name, task_list->comm))
+    {
+      printk(KERN_INFO "Found process %s with pid %d", task_list->comm, task_list->pid);
+      return 0;
+    }
   }
-  printk(KERN_INFO "Module Loaded");
+  printk(KERN_INFO "Not Found process %s", name);
+
   return 0;
 }
 static void __exit find_task_exit(void) {
